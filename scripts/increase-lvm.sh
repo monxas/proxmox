af8b7b168992ae5355dbafe1d0fee6e180dc27bb
monxas <ramonkawa@gmail.com>
2023-04-29 16:27:38 +0200

    Update increase-lvm.sh

--- scripts/increase-lvm.sh
+++ scripts/increase-lvm.sh
@@ -1,44 +1,36 @@
 #!/bin/bash
 set -e
 
-# Function to display available devices
-list_devices() {
-  lsblk -d -o NAME,SIZE,TYPE | grep "disk"
+# Function to list available devices
+function list_devices() {
+    lsblk -dpno NAME,SIZE,TYPE | grep -w "disk" | awk '{print $1 " " $2}'
 }
 
-# Function to display available logical volumes
-list_logical_volumes() {
-  lvdisplay --columns --separator ',' --noheading -o lv_name,vg_name | awk -F ',' '{print "/dev/"$2"/"$1}'
+# Function to list available logical volumes
+function list_logical_volumes() {
+    lvdisplay | grep "LV Path" | awk '{print $3}'
 }
 
-# Prompt the user to select a device
+# Prompt user to select a device
 echo "Available devices:"
 list_devices
-read -p "Enter the device to resize (e.g., /dev/sda): " device
+echo
+read -p "Enter the device you want to resize (e.g. /dev/sda): " device
 
-# Prompt the user to select a logical volume
+# Prompt user to select a logical volume
 echo "Available logical volumes:"
 list_logical_volumes
-read -p "Enter the root logical volume path (e.g., /dev/ubuntu-vg/ubuntu-lv): " lv_root
+echo
+read -p "Enter the logical volume you want to resize (e.g. /dev/ubuntu-vg/ubuntu-lv): " lv_root
 
-# Variables
-partition_number="3"
+# Prompt user to enter the desired size in GB to increase
+read -p "Enter the size in GB to increase the logical volume: " size_gb
+
+# Get the partition number
+partition_number=$(lsblk -o NAME,TYPE,MOUNTPOINT | grep -w "lvm" | head -n 1 | cut -d"p" -f2)
+
+# Set the partition type (Linux LVM)
 partition_type="30"
-
-# Prompt the user for the size to increase in GB
-read -p "Enter the size to increase in GB: " increase_size
-
-# Login as root if needed (not needed for proxmox)
-sudo su
-
-# List disks and partitions
-fdisk -l
-
-# List volume groups
-vgdisplay
-
-# List logical volumes
-lvdisplay
 
 # Edit partitions with fdisk, change device id as needed
 (
@@ -63,11 +55,11 @@ fdisk -l
 # Extend the existing physical volume
 pvresize ${device}${partition_number}
 
-# Extend the root logical volume with the user-specified size
-lvresize -L +${increase_size}G $lv_root
+# Extend the selected logical volume with the desired size
+lvresize -L +${size_gb}G $lv_root
 
 # Extend the underlying file system
 resize2fs $lv_root
 
-# List logical volumes, noting root is now larger by the specified size
+# List logical volumes, noting the increased size
 lvdisplay
