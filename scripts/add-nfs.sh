5aa2094cb5505fec6ea0ea40fb9e3379c08219d2
monxas <ramonkawa@gmail.com>
2024-05-30 17:49:37 +0200

    Update add-nfs.sh

--- scripts/add-nfs.sh
+++ scripts/add-nfs.sh
@@ -1,5 +1,6 @@
 # one liner 
 # sudo apt update && sudo apt install -y nfs-common && sudo mkdir -p /mnt/nfs_media && echo '192.168.0.237:/Volume1/Media /mnt/nfs_media nfs rw,relatime,vers=4.1,nofail 0 0' | sudo tee -a /etc/fstab && sudo mount -a
+# curl -sSL https://raw.githubusercontent.com/monxas/proxmox/develop/scripts/add-nfs.sh | bash
 
 #!/bin/bash
 
